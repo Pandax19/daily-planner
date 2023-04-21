4b2a2840eef623498d3d377b214b5a377761688e
Pandax19 <ajwhitley@comcast.net>
2023-04-21 00:28:36 -0400

    got the text to stay on page after reset but couldn't stop text from moving to top of schedule

--- assets/script.js
+++ assets/script.js
@@ -16,28 +16,34 @@ let task = taskEl.val();
 //   taskEl.addClass('col-8 col-md-10 description');
 //   taskEl.appendTo(entry);
 // }
-entry = localStorage.getItem('save');
+// entry = localStorage.getItem('save');
 var todos = [];
-let savedTodos = {
-  "time" : taskHour,
-  "task" : task
-};
+// let savedTodos = {
+//   "time" : taskHour,
+//   "task" : task
+// };
 
 container.on("click", ".saveBtn", function(event){
 let taskDiv = $(event.target).parent().val($(this).attr("data-time"));
-  console.log("taskDiv", taskDiv);
+  // console.log("taskDiv", taskDiv);
   taskHour = parseInt(taskDiv.attr("data-time"));
   console.log("taskHour", taskHour);
   task = taskDiv.children(".description").val();
   console.log(task);
-  savedTodos = {
+  let savedTodos = {
     "time" : taskHour,
     "task" : task
   }
-  // event.push("savedTasks")
-  localStorage.setItem("savedTasks", JSON.stringify(savedTodos));
   storedTasks = JSON.parse(localStorage.getItem("savedTasks"));
-  
+  if (storedTasks !== null) {
+    todos = storedTasks
+  }
+  todos.push(savedTodos)
+  localStorage.setItem("savedTasks", JSON.stringify(todos));
+  //  todos.push(storedTasks);
+  // console.log(storedTasks);
+  // console.log(todos);
+  // todos.push();
 });
 //  push savedTodos
 
@@ -45,42 +51,55 @@ let storedTasks = JSON.parse(localStorage.getItem("savedTasks"));
 
 for (var i = 0; i < task.length; i++)
   console.log(task[i]);
-// renderTodos
 
-
-function render(savedTasks){
-  // todoList.innerHTML = "";
-  $("[name=savedTasks]").val(savedTasks);
-  storedTasks = JSON.parse(localStorage.getItem("savedTasks"));
-  savedTasks.push(savedTasks, "savedTodos");
-  }
+// function render(savedTasks){
+//   // todoList.innerHTML = "";
+//   $("[name=savedTasks]").val(savedTasks);
+//   storedTasks = JSON.parse(localStorage.getItem("savedTasks"));
+//   savedTasks.push(savedTasks, "savedTodos");
+//   }
 // 
-  // 
-// }
+// // }
 function displaySchedule() { 
-  let storedSchedule = JSON.parse(localStorage.getItem("schedule"));
+  let storedSchedule = JSON.parse(localStorage.getItem("savedTasks"));
+  console.log(storedSchedule);
+  // todos.push(storedSchedule);
+  if (storedSchedule !== null) {
+    todos = storedSchedule
+  }
+
   let scheduleBoxes = document.querySelectorAll('.description');
-  for (i = 0; i < storedSchedule.length; i++) {
-  scheduleBoxes[i].textContent = storedSchedule[i].task
-  }};
-  
-// init ();
-$(function () {
-});
+  console.log(todos);
+
+  console.log(scheduleBoxes);
+  for (i = 0; i < todos.length; i++) {
+    // if (todos[i].time === timeArray[i]) {
+      console.log($(`#${i}`).children(".description").val(todos[i].task));
+    console.log(todos);
+    // console.log(todos[i].time);
+  //   console.log(todos[i]);
+  //   console.log(timeArray[i]);
+  // scheduleBoxes[i].textContent = todos[i].task
+
+    }
+  };
+  displaySchedule();
 
 let handleFormSubmit = function (event) {
   event.preventDefault(); 
   
-  // event.push(savedTasks, "savedTodos");
-  // render savedTasks
-//   var unicorn = localStorage.getItem("uinicorn-0")
-//   if(unicorn){
-//     unicorn = JSON.parse(unicorn);
-//     $container("[name=unicorn-name]").val(unicorn.unicornName);
-//     console.log(unicorn);
-  // todoList.innerHTML = "";
-  // todoCountSpan.textContent = todos.length;
-  // event.push(savedTasks, "savedTodos");
+
+
+// event.push(savedTasks, "savedTodos");
+// render savedTasks
+// var unicorn = localStorage.getItem("uinicorn-0")
+// if(unicorn){
+// unicorn = JSON.parse(unicorn);
+// $container("[name=unicorn-name]").val(unicorn.unicornName);
+// console.log(unicorn);
+// todoList.innerHTML = "";
+// todoCountSpan.textContent = todos.length;
+// event.push(savedTasks, "savedTodos");
 }
 // // The following function renders items in a todo list as <li> elements
 // function renderTodos() {
